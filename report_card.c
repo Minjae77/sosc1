f69c7d21ea57ed16faa96dd178a092ce8ddc0e0d
MinjaeLyou <lminjae0313@gmail.com>
2018-10-02 19:29:31 +0900

    report card: Get a mean of grades

--- report_card.c
+++ report_card.c
@@ -14,7 +14,8 @@ int main()
 
 	printf("Korean : %d\n", kor);
 	printf("English : %d\n", eng);
-	printf("Math : %d\n", math);
+	printf("Mean : %d\n", math);
 	printf("Sum : %d\n", sum);
+	printf("Mean : %d\n", sum/3);
 	return 0;
 }
